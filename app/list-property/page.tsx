bfb4d56284c0139460cf292cf07817fa799b4fda
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2026-05-14 08:00:24 +0100

    added hotel

--- app/list-property/page.tsx
+++ app/list-property/page.tsx
@@ -430,6 +430,7 @@ export default function ListPropertyPage() {
               <option value="Commercial Property">Commercial Property</option>
               <option value="Land">Land</option>
               <option value="Office">Office</option>
+               <option value="Hotel">Hotel</option>
               <option value="Other">Other</option>
               <option value="Guest House">Guest House</option>
             </select>
